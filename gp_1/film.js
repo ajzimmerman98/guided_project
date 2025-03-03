e48fb7a7907e0d19fd1faccfa5f16664cbbb9f28
AJ Zimmerman <ajzimmerman98@gmail.com>
2025-03-03 13:54:24 -0500

    working urls

--- gp_1/film.js
+++ gp_1/film.js
@@ -1,8 +1,33 @@
 const sp = new URLSearchParams(window.location.search);
+console.log("sp:", sp); // Log the sp object directly
 const id = sp.get("id");
+console.log("id:", id); // Log the id value directly
 
-async function getFilms(id) {
-  const url = `http://localhost:9001/api/films/${id}`;
+// let producer;
+// let title;
+// let episode_id;
+// let director;
+// let release_date;
+// let opening_crawl;
+// let charactersUl;
+// let planetsUl;
+const baseUrl = `http://localhost:9001/api/films`;
+
+// addEventListener('DOMContentLoaded', () => {
+//   producer = document.querySelector('span#Producer');
+//   title = document.querySelector('h1#name');
+//   episode_id = document.querySelector('span#Episode');
+//   director = document.querySelector('span#Director');
+//   release_date = document.querySelector('span#Released');
+//   opening_crawl = document.querySelector('span#OpeningCrawl');
+//   charactersUl = document.querySelector('ul#Characters_ID');
+//   planetsUl = document.querySelector('ul#Planets_ID');
+//   const sp = new URLSearchParams(window.location.search);
+//   const id = sp.get('id');
+// });
+
+async function getFilms() {
+  const url = `${baseUrl}/${id}`;
 
   try {
     const response = await fetch(url);
@@ -19,7 +44,7 @@ async function getFilms(id) {
 }
 
 const getCharacters = async function (id) {
-  const url = `http://localhost:9001/api/${id}/characters`;
+  const url = `${baseUrl}/${id}/characters`;
 
   try {
     const response = await fetch(url);
@@ -29,14 +54,18 @@ const getCharacters = async function (id) {
     }
 
     const json = await response.json();
-    console.log(json);
+    const characterArrays = json.map((character) => [
+      character.id,
+      character.name,
+    ]);
+    console.log(characterArrays);
   } catch (error) {
     console.error(error.message);
   }
 };
 
 const getPlanets = async function (id) {
-  const url = `http://localhost:9001/api/films/${id}/planets`;
+  const url = `${baseUrl}/${id}/planets`;
 
   try {
     const response = await fetch(url);
